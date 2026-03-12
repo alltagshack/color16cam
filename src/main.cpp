c2a263ccd80d1af6ede4889944869461c98e367f
jochen@homeland <no-go@localhost>
2026-03-12 13:32:32 +0100

    new pinout

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,35 @@
 #include "esp_camera.h"
 #include "img_converters.h"
 
-#define PICTURE_BUTTON      41
+// Printer
+// RX <- UART TX            43
+// TX <- UART RX            44 (unused)
+
+#define PICTURE_BUTTON      42
 
 // OLED
-#define PIN_CS              21
-#define PIN_RST             14
+#define PIN_RST             41
+//      CLK <-      SCK     36
+//      DIN <-      MOSI    35
 #define PIN_DC              47
+#define PIN_CS              21
 
 // ov3660 CAM
 #define PWDN_GPIO_NUM       -1
 #define RESET_GPIO_NUM      -1
-#define XCLK_GPIO_NUM       15
 #define SIOD_GPIO_NUM        4
 #define SIOC_GPIO_NUM        5
-#define Y2_GPIO_NUM         11
-#define Y3_GPIO_NUM          9
-#define Y4_GPIO_NUM          8
-#define Y5_GPIO_NUM         10
-#define Y6_GPIO_NUM         12
-#define Y7_GPIO_NUM         18
-#define Y8_GPIO_NUM         17
-#define Y9_GPIO_NUM         16
 #define VSYNC_GPIO_NUM       6
 #define HREF_GPIO_NUM        7
+#define XCLK_GPIO_NUM       15
+#define Y9_GPIO_NUM         16
+#define Y8_GPIO_NUM         17
+#define Y7_GPIO_NUM         18
+#define Y4_GPIO_NUM          8
+#define Y3_GPIO_NUM          9
+#define Y5_GPIO_NUM         10
+#define Y2_GPIO_NUM         11
+#define Y6_GPIO_NUM         12
 #define PCLK_GPIO_NUM       13
 
 #include <Adafruit_GFX.h>
